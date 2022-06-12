d6d43e3203de1afa47c5508bb48f8fb2376d7931
Ezekiel <ezekielchow94@gmail.com>
2022-06-12 21:09:32 +0800

    Halfway registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,32 @@
+var passport = require('passport')
 
 exports.getRegister = (req, res, next) => {
     res.render('register');
 };
+
+exports.postRegister = (req, res, next) => {
+    console.log('post reg', req.body);
+
+    try {
+        const email = req.body.email
+        const password = req.body.password
+        const passwordRepeat = req.body.password_repeat
+
+        if (!email || !password || !passwordRepeat) {
+            throw "Missing fields"
+        }
+
+        if (password !== passwordRepeat) {
+            throw "Passwords doesn't match"
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.postLogin = (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    })
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,38 @@
 const express = require('express')
+var passport = require('passport')
+var LocalStrategy = require('passport-local')
 
 const authController = require('../controllers/auth')
+const User = require('../models/user')
+
+passport.use(new LocalStrategy(async function verify(email, password, cb) {
+
+    try {
+        const user = await User.find({ email })
+
+        if (!user) {
+            return cb(null, false, { message: 'Incorrect username or password.' });
+        }
+
+        crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', function (err, hashedPassword) {
+            if (err) { return cb(err); }
+            if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+                return cb(null, false, { message: 'Incorrect username or password.' });
+            }
+            return cb(null, user);
+        });
+
+    } catch (error) {
+        cb(err)
+    }
+
+}));
+
 
 const router = express.Router()
 
 router.get('/register', authController.getRegister)
+router.post('/register', authController.postRegister)
+router.post('/login', authController.postLogin)
 
 module.exports = router;
