4e966fe85a49e993ec3e7be6ae9e1d4fa9ce5508
Ezekiel <ezekielchow94@gmail.com>
2022-06-14 15:33:03 +0800

    Login works

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
-var passport = require('passport')
 const { body, validationResult } = require('express-validator');
+const crypto = require('crypto')
 
 const validators = require('../helpers/validators')
+const User = require('../models/user')
 
 exports.getRegister = (req, res, next) => {
     res.render('register');
 };
 
-exports.postRegister = (req, res, next) => {
+exports.postRegister = async (req, res, next) => {
 
     try {
         const errors = validationResult(req);
@@ -19,18 +20,55 @@ exports.postRegister = (req, res, next) => {
             return res.redirect('back')
         }
 
-        return res.redirect('back')
+        const user = new User(req.body)
+
+        const salt = await crypto.randomBytes(128).toString('hex')
+        const password = await crypto.pbkdf2Sync(
+            req.body.password, salt, 1000, 64, 'sha512').toString('hex')
+
+        user.salt = salt
+        user.password = password
+
+        await user.save()
+
+        req.flash('messageSuccess', 'User successfully registered')
+        return res.redirect('/login')
 
     } catch (error) {
-        console.log(error);
+        req.flash('oldForm', req.body)
+        req.flash('messageFailure', error)
+        return res.redirect('back')
     }
 }
 
-exports.postLogin = (req, res, next) => {
-    passport.authenticate('local', {
-        successRedirect: '/',
-        failureRedirect: '/login'
-    })
+exports.getLogin = (req, res, next) => {
+    res.render('login')
+}
+
+exports.postLogin = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email }).exec()
+
+        if (!user) {
+            req.flash('oldForm', req.body)
+            req.flash('messageFailure', 'User not found')
+            return res.redirect('back')
+        }
+
+        const hashedPassword = await crypto.pbkdf2Sync(req.body.password, user.salt, 1000, 64, 'sha512').toString('hex');
+
+        if (user.password !== hashedPassword) {
+            req.flash('oldForm', req.body)
+            req.flash('messageFailure', 'Incorrect username or password')
+            return res.redirect('back')
+        }
+
+        return res.redirect('/dashboard')
+    } catch (error) {
+        req.flash('oldForm', req.body)
+        req.flash('messageFailure', error)
+        return res.redirect('back')
+    }
 }
 
 exports.validate = (method) => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,6 @@
 const express = require('express')
-var passport = require('passport')
-var LocalStrategy = require('passport-local')
 
 const authController = require('../controllers/auth')
-const User = require('../models/user')
-
-passport.use(new LocalStrategy(async function verify(email, password, cb) {
-
-    try {
-        const user = await User.find({ email })
-
-        if (!user) {
-            return cb(null, false, { message: 'Incorrect username or password.' });
-        }
-
-        crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', function (err, hashedPassword) {
-            if (err) { return cb(err); }
-            if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
-                return cb(null, false, { message: 'Incorrect username or password.' });
-            }
-            return cb(null, user);
-        });
-
-    } catch (error) {
-        cb(err)
-    }
-
-}));
-
 
 const router = express.Router()
 
@@ -35,6 +8,7 @@ router.get('/register', authController.getRegister)
 router.post('/register',
     authController.validate('postRegister'),
     authController.postRegister)
+router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
 
 module.exports = router;
