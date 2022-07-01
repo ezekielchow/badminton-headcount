4e7b9b8348148d9c0718a41d334271249513a589
Ezekiel <ezekielchow94@gmail.com>
2022-07-01 12:49:51 +0800

    Remove view routes. will use nuxtjs

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,11 @@ const authController = require('../controllers/auth')
 
 const router = express.Router()
 
-router.get('/register', authController.getRegister)
+// router.get('/register', authController.getRegister)
 router.post('/register',
     authController.validate('postRegister'),
     authController.postRegister)
-router.get('/login', authController.getLogin)
+// router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
 
 module.exports = router;
--- routes/headcount.js
+++ routes/headcount.js
@@ -5,10 +5,10 @@ const isAuthenticated = require("../middlewares/isAuthenticated");
 
 const router = express.Router()
 
-router.get('/dashboard', isAuthenticated, headcountController.getDashboard)
-router.get('/onboarding', isAuthenticated, headcountController.getOnboarding)
+// router.get('/dashboard', isAuthenticated, headcountController.getDashboard)
+// router.get('/onboarding', isAuthenticated, headcountController.getOnboarding)
 router.post('/onboarding', isAuthenticated, headcountController.validate('postOnboarding'), headcountController.postOnboarding)
-router.get('/add-headcount', isAuthenticated, headcountController.getHeadcountPage)
+// router.get('/add-headcount', isAuthenticated, headcountController.getHeadcountPage)
 router.post('/add-headcount', isAuthenticated, headcountController.validate('postHeadcount'), headcountController.postHeadcount)
 
 module.exports = router;
